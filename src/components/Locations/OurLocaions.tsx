599de6051443e268feccbad770b2c4341926067e
Uneeb Bhatti <uneebbhatti3@gmail.com>
2025-01-06 00:18:12 +0500

    new slider added

--- src/components/Locations/OurLocaions.tsx
+++ src/components/Locations/OurLocaions.tsx
@@ -55,7 +55,7 @@ const OurLocaions = () => {
               >
                 Visit Website
               </Link>
-              <Slider />
+              <LocationSlider />
             </div>
           </div>
 
